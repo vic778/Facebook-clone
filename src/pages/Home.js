dd7d5ff262a6845f712c364fdc1b2a902fe78745
vic778 <victoiremmanuelbarh@gmail.com>
2022-01-02 17:15:02 +0200

    Ignored node_modules

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Topbar from '../components/Topbar';
+
+import '../styles/Home.css'
+
+const Home = () => {
+    return (
+        <div>
+           <Topbar />
+        </div>
+    );
+};
+
+export default Home;
