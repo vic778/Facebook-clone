b58c56c2f71f5859731dfb7044b3490c61fa4f26
vic778 <victoiremmanuelbarh@gmail.com>
2022-01-02 18:34:11 +0200

    Uploded my picture as a profil

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -33,7 +33,7 @@ const Topbar = () => {
                         <span className="topbarIconBadge">1</span>
                     </div>
                 </div>
-                <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+                <img src="/assets/person/vic.jpg" alt="" className="topbarImg" />
             </div> 
             
         </div>
