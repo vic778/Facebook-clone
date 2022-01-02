292591e9b5339fbe2470caeaf7c063184618d914
vic778 <victoiremmanuelbarh@gmail.com>
2022-01-02 18:27:33 +0200

    Added homeContaine div to diplay items

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Feed from '../components/Feed';
+import Rightbar from '../components/Rightbar';
+import Sidebar from '../components/Sidebar';
 import Topbar from '../components/Topbar';
+import '../styles/Home.css';
 
-import '../styles/Home.css'
 
 const Home = () => {
     return (
         <div>
            <Topbar />
+           <div className="homeContainer">
+                <Sidebar />
+                <Feed />
+                <Rightbar />
+           </div>
+          
         </div>
     );
 };
