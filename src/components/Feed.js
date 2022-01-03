97d1c914570abdec03b02423824624ad6c808565
vic778 <victoiremmanuelbarh@gmail.com>
2022-01-03 13:39:48 +0200

    Imported post component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../styles/FeedStyle.css';
+import Post from './Post';
+import Share from './Share';
 
 const Feed = () => {
     return (
         <div className='feed'>
-            feed
+            <div className="feedWrapper">
+                <Share />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </div>
         </div>
     );
 };
