96a1145096de8b31758f755f81446d7ac844ace2
vic778 <victoiremmanuelbarh@gmail.com>
2022-01-04 09:30:47 +0200

    Added online section with images and friends birthdays

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -4,7 +4,72 @@ import '../styles/RightStyle.css';
 const Rightbar = () => {
     return (
         <div className='rightbar'>
-            rightbar
+           <div className="rightbarWrapper">
+               <div className="birthdayContainer">
+                   <img className='birthdayImg' src="assets/gift.png" alt="" />
+                   <span className="birthdayText"><b>Victor Barh and </b><b>3 other friends</b> have a birthday tody</span>
+               </div>
+               <img className='rightbarAd' src="assets/ad.png" alt="" />
+               <h4 className='rightbarTitle'>Online Friends</h4>
+               <ul className="rightbarFriendList">
+                   <li className="rightbarFriend">
+                       <div className="rightbarhProfilImgContainer">
+                           <img src="assets/person/vic.jpg" alt="" className="rightbarProfilImg" />
+                           <span className='rightbarOnline'></span>
+                       </div>
+                       <span className="rightbarUserName">Victor Barh</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarhProfilImgContainer">
+                           <img src="assets/person/vic.jpg" alt="" className="rightbarProfilImg" />
+                           <span className='rightbarOnline'></span>
+                       </div>
+                       <span className="rightbarUserName">Victor Barh</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarhProfilImgContainer">
+                           <img src="assets/person/vic.jpg" alt="" className="rightbarProfilImg" />
+                           <span className='rightbarOnline'></span>
+                       </div>
+                       <span className="rightbarUserName">Victor Barh</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarhProfilImgContainer">
+                           <img src="assets/person/vic.jpg" alt="" className="rightbarProfilImg" />
+                           <span className='rightbarOnline'></span>
+                       </div>
+                       <span className="rightbarUserName">Victor Barh</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarhProfilImgContainer">
+                           <img src="assets/person/vic.jpg" alt="" className="rightbarProfilImg" />
+                           <span className='rightbarOnline'></span>
+                       </div>
+                       <span className="rightbarUserName">Victor Barh</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarhProfilImgContainer">
+                           <img src="assets/person/vic.jpg" alt="" className="rightbarProfilImg" />
+                           <span className='rightbarOnline'></span>
+                       </div>
+                       <span className="rightbarUserName">Victor Barh</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarhProfilImgContainer">
+                           <img src="assets/person/vic.jpg" alt="" className="rightbarProfilImg" />
+                           <span className='rightbarOnline'></span>
+                       </div>
+                       <span className="rightbarUserName">Victor Barh</span>
+                   </li>
+                   <li className="rightbarFriend">
+                       <div className="rightbarhProfilImgContainer">
+                           <img src="assets/person/vic.jpg" alt="" className="rightbarProfilImg" />
+                           <span className='rightbarOnline'></span>
+                       </div>
+                       <span className="rightbarUserName">Victor Barh</span>
+                   </li>
+               </ul>
+           </div>
         </div>
     );
 };
